b3cafb8de9b72b85261807d8899bedf493b58891
AkashItlogiko <akashsahanwu089@gmail.com>
2025-03-24 11:17:18 +0600

    Done

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Admin;
+// use App\Models\Admin;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -17,7 +17,7 @@ class AdminFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    protected $model = Admin::class;
+    // protected $model = Admin::class;
      public function definition(): array
     {
         return [
