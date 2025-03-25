b11f027f09fe2c0572fe6b3bb67350f304c476ab
AkashItlogiko <akashsahanwu089@gmail.com>
2025-03-25 13:27:13 +0600

    Done

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\AdminColorController;  
 
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
 
-Route::get('/',[AdminController::class,'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
 
-Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
+Route::middleware('admin')->group(function () {
+    Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
+    Route::post('admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
 
-Route::middleware('admin')->group(function(){
-Route::get('admin/dashboard',[AdminController::class,'index'])->name('admin.index');
-Route::post('admin/logout',[AdminController::class,'logout'])->name('admin.logout');
+    // Colors routes
+    Route::resource('colors', AdminColorController::class);
 });
